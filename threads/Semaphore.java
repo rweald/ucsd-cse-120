b35f905379ccd11ea9286a0f58cf9af12bc2cdca
Ryan Weald <ryan@weald.com>
2011-10-16 12:08:20 -0700

    fix some indentation

--- threads/Semaphore.java
+++ threads/Semaphore.java
@@ -22,83 +22,83 @@ import nachos.machine.*;
  * true value might now be different.
  */
 public class Semaphore {
-    /**
-     * Allocate a new semaphore.
-     *
-     * @param	initialValue	the initial value of this semaphore.
-     */
-    public Semaphore(int initialValue) {
-	value = initialValue;
+  /**
+   * Allocate a new semaphore.
+   *
+   * @param	initialValue	the initial value of this semaphore.
+   */
+  public Semaphore(int initialValue) {
+    value = initialValue;
+  }
+
+  /**
+   * Atomically wait for this semaphore to become non-zero and decrement it.
+   */
+  public void P() {
+    boolean intStatus = Machine.interrupt().disable();
+
+    if (value == 0) {
+      waitQueue.waitForAccess(KThread.currentThread());
+      KThread.sleep();
+    }
+    else {
+      value--;
     }
 
-    /**
-     * Atomically wait for this semaphore to become non-zero and decrement it.
-     */
-    public void P() {
-	boolean intStatus = Machine.interrupt().disable();
+    Machine.interrupt().restore(intStatus);
+  }
 
-	if (value == 0) {
-	    waitQueue.waitForAccess(KThread.currentThread());
-	    KThread.sleep();
-	}
-	else {
-	    value--;
-	}
+  /**
+   * Atomically increment this semaphore and wake up at most one other thread
+   * sleeping on this semaphore.
+   */
+  public void V() {
+    boolean intStatus = Machine.interrupt().disable();
 
-	Machine.interrupt().restore(intStatus);
+    KThread thread = waitQueue.nextThread();
+    if (thread != null) {
+      thread.ready();
+    }
+    else {
+      value++;
     }
 
-    /**
-     * Atomically increment this semaphore and wake up at most one other thread
-     * sleeping on this semaphore.
-     */
-    public void V() {
-	boolean intStatus = Machine.interrupt().disable();
+    Machine.interrupt().restore(intStatus);
+  }
 
-	KThread thread = waitQueue.nextThread();
-	if (thread != null) {
-	    thread.ready();
-	}
-	else {
-	    value++;
-	}
-	
-	Machine.interrupt().restore(intStatus);
+  private static class PingTest implements Runnable {
+    PingTest(Semaphore ping, Semaphore pong) {
+      this.ping = ping;
+      this.pong = pong;
     }
 
-    private static class PingTest implements Runnable {
-	PingTest(Semaphore ping, Semaphore pong) {
-	    this.ping = ping;
-	    this.pong = pong;
-	}
-	
-	public void run() {
-	    for (int i=0; i<10; i++) {
-		ping.P();
-		pong.V();
-	    }
-	}
-
-	private Semaphore ping;
-	private Semaphore pong;
+    public void run() {
+      for (int i=0; i<10; i++) {
+        ping.P();
+        pong.V();
+      }
     }
 
-    /**
-     * Test if this module is working.
-     */
-    public static void selfTest() {
-	Semaphore ping = new Semaphore(0);
-	Semaphore pong = new Semaphore(0);
+    private Semaphore ping;
+    private Semaphore pong;
+  }
 
-	new KThread(new PingTest(ping, pong)).setName("ping").fork();
+  /**
+   * Test if this module is working.
+   */
+  public static void selfTest() {
+    Semaphore ping = new Semaphore(0);
+    Semaphore pong = new Semaphore(0);
 
-	for (int i=0; i<10; i++) {
-	    ping.V();
-	    pong.P();
-	}
+    new KThread(new PingTest(ping, pong)).setName("ping").fork();
+
+    for (int i=0; i<10; i++) {
+      ping.V();
+      pong.P();
     }
+  }
 
-    private int value;
-    private ThreadQueue waitQueue =
-	ThreadedKernel.scheduler.newThreadQueue(false);
+  private int value;
+  private ThreadQueue waitQueue =
+    ThreadedKernel.scheduler.newThreadQueue(false);
 }
